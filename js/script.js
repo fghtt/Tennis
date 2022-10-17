eda8e6ebaf4dcc6393f7cb16994cc00d6fe7cdb5
Farrukh <69960319+fghtt@users.noreply.github.com>
2022-10-17 22:55:53 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-
-/*setInterval(function(){
-	if(window.pageYOffset >= 90){
-		$('.top-menu').css('position', 'fixed');
-		$('.top-menu').css('z-index', '3');
-		$('.top-menu').css('background', '#8D86C9');
-	}else{
-		$('.top-menu').css('position', 'relative');
-		$('.top-menu').css('z-index', '0');
-		$('.top-menu').css('background', '');
-	}
-}, 10)*/
-
 $(window).scroll(function(){
 	if($(this).scrollTop() > 90){
 		$('.top-menu').css('position', 'fixed')
